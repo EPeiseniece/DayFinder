46a1239edafa2db291ec22fc81d16d576c04ee57
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-09 23:00:04 +0300

    Surprise Screen

---
+++ DayFinder/Controler/SurpriseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SurpriseViewController.swift
+//  DayFinder
+//
+//  Created by elina.peiseniece on 09/08/2021.
+//
+
+import UIKit
+
+class SurpriseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
